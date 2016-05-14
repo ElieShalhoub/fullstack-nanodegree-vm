745b0f433e82b51c4efe61e64834f2c8e664411b
Elie Shalhoub <elie.shalhoub@outlook.com>
2016-05-15 02:00:55 +0300

    Add flashing and static folder

--- vagrant/webserver_dev/restaurants/project.py
+++ vagrant/webserver_dev/restaurants/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -54,6 +54,7 @@ def newMenuItem(restaurant_id):
         newItem = MenuItem(name = request.form['name'],restaurant_id=restaurant_id)
         session.add(newItem)
         session.commit()
+        flash('New item [%s] inserted' %newItem.name)
         return redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html',restaurant_id=restaurant_id)
@@ -68,6 +69,7 @@ def editMenuItem(restaurant_id, menu_id):
             editItem.name = request.form['name']
         session.add(editItem)
         session.commit()
+        flash('%s has been edited' %editItem.name)
         return redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
     else:
         return render_template('editmenuitem.html',restaurant_id=restaurant_id,menu_id=menu_id,item=editItem)
@@ -79,11 +81,13 @@ def deleteMenuItem(restaurant_id, menu_id):
     if request.method == 'POST':
         session.delete(delItem)
         session.commit()
+        flash('%s has been deleted' %delItem.name)
         return redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
     else:
         return render_template('deletemenuitem.html',restaurant_id=restaurant_id,menu_id=menu_id,item=delItem)
 
 
 if __name__ == '__main__':
+    app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0',port=8081)
