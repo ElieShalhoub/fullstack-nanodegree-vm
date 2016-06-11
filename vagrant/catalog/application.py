ab4f4853857369f4956823e2fd4eb45c7bc4b058
Elie Shalhoub <elie.shalhoub@outlook.com>
2016-06-11 21:14:18 +0300

    Add database setup

--- vagrant/catalog/application.py
+++ vagrant/catalog/application.py
@@ -1,14 +1,37 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Category, CategoryItem
 
 app = Flask(__name__)
 
+engine = create_engine('sqlite:///catalog.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 #Default route
 @app.route('/')
-@app.route('/catalog')
+@app.route('/category')
 def defaultCatalog():
-    #restaurant = session.query(Restaurant).first()
-    #items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
-    return "catalog home" 
+    category = session.query(Category).first()
+    return render_template('category.html', category = category)
+ 
+
+@app.route('/category/new' , methods=['GET','POST'])
+def newCatalog():
+	#if request.method == 'POST':
+		newCat = Category(name = "Football")
+		session.add(newCat)
+		session.commit()
+		#flash('New category [%s] inserted' %category.name)
+		#return redirect(url_for('restaurantMenu',restaurant_id=restaurant_id))
+		return "added successfully"
+	#else:
+	#return render_template('newmenuitem.html',restaurant_id=restaurant_id
+	#	return "___"
+
 
 #launching the application
 if __name__ == '__main__':
